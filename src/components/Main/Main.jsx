214a0b46a3745f6e1c2945a6b405733a7db75d48
SuryaPS3 <spsingh2003.sps@gmail.com>
2025-11-11 13:26:01 +0530

    Created the UI of our project. closes #2 closes #1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -22,24 +22,33 @@ const Main = () => {
                         <p>Suggest something about routines</p>
                         <img src={assets.compass_icon} alt="" />
                     </div>
-                </div>
-                <div className="cards">
+                
                     <div className="card">
                         <p>Suggest something about routines</p>
                         <img src={assets.bulb_icon} alt="" />
                     </div>
-                </div>
-                <div className="cards">
+                
                     <div className="card">
                         <p>Suggest something about routines</p>
                         <img src={assets.message_icon} alt="" />
                     </div>
-                </div>
-                <div className="cards">
+                
                     <div className="card">
                         <p>Suggest something about routines</p>
                         <img src={assets.code_icon} alt="" />
                     </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder="Type your message here..." />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="info">
+                        Gemini is an AI-powered assistant that helps you with a variety of tasks, from answering questions to generating content.                     </p>
                 </div>
             </div>
         </div>
