6ba6d7b5b21a917df57a634ca19d858ccf0a57c9
SuryaPS3 <spsingh2003.sps@gmail.com>
2025-11-11 10:49:51 +0530

    Added the html and icons for the sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Sidebar.css';
+import {assets} from '../../assets/assets.js';
+
+const Sidebar = () =>{
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <img src={assets.menu_icon} alt="Logo" />
+            
+            <div className="new-chat">
+                <img src={assets.plus_icon} alt="" />
+                <p>New Chat</p>
+            </div>
+            <div className="recent">
+                <p className="recent-title"></p>
+                <div className="recent-chats">
+                    <img src={assets.message_icon} alt="" />
+                </div>
+            </div>
+              </div>
+            
+            <div className="bottom">
+                <div className="bottom-item recent-entry">
+                    <img src={assets.question_icon} alt="" />
+                    <p>Help</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.history_icon} alt="" />
+                    <p>Activity</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.setting_icon} alt="" />
+                    <p>Settings</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
